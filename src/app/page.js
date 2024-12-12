7b74bf6b9f7c8ed1db491445585790c3702ba3f6
Noisk8 <juansk8jaramillo@gmail.com>
2024-12-12 04:10:47 -0500

    add shadcn y cambio de tema

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,103 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
 import Image from "next/image";
+import { Button}  from "@/components/ui/button";
+import  {Input}  from "@/components/ui/input";
+import {ThemeSwitcher} from "@/components/themeSwitcher";
+
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 export default function Home() {
+  const urlBase = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  const difKelvin = 273.15;
+
+  const handleSub = (e) => {
+    e.preventDefault();
+    if (ciudad.length > 0) fetchClima();
+  };
+
+  const [ciudad, setCuidad] = useState("");
+  const [dataClima, setDataClima] = useState(null);
+
+  const handleCambioCiudad = (e) => {
+    setCuidad(e.target.value);
+  };
+
+  const fetchClima = async () => {
+    try {
+      const response = await fetch(`${urlBase}?q=${ciudad}&appid=${API_KEY}`);
+      const data = await response.json();
+      setDataClima(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+        <header className="row-start-1">
+  <ThemeSwitcher />
+      </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="text-4xl font-bold text-center">Clima</h1>
+        <form
+          className="flex flex-col gap-4 items-center sm:items-start"
+          onSubmit={handleSub}
+        >
+          <Input
+            type="text"
+            value={ciudad}
+            onChange={handleCambioCiudad}
+            placeholder="Ingrese una ciudad"
+            className=" text-black"
+          />
+          <Button type="submit">Buscar</Button>
+        </form>
+     
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {dataClima && (
+            <div className="weather-container">
+              <Card>
+                <CardHeader>
+                  <CardTitle>{dataClima.name}</CardTitle>
+                  <CardDescription>
+                    <p>{dataClima.sys?.country}</p>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="font-bold"> Temperatura: </p>{" "}
+                  <p>{parseInt(dataClima?.main?.temp - difKelvin)}°c </p>
+                  <Image
+                    src={`http://openweathermap.org/img/w/${dataClima.weather[0].icon}.png`}
+                    width={100}
+                    height={100}
+                    alt="clima"
+                    className="weather-icon"
+                  />
+                </CardContent>
+                <CardFooter>
+                  <p className="font-bold">Condición Meteorologíca:</p>{" "}
+                  
+                </CardFooter>
+                <CardFooter>  <p> {dataClima.weather[0].description}</p></CardFooter>
+              </Card>
+            </div>
+          )}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
